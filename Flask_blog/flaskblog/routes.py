ad577bb24131f1d14ef07f8cc366ef94ef5a2acb
Pawel <pawnowak95@gmail.com>
2020-02-19 20:39:09 +0100

    cokolwiek

--- Flask_blog/flaskblog/routes.py
+++ Flask_blog/flaskblog/routes.py
@@ -118,6 +118,9 @@ def update_post(post_id):
         post.content = form.content.data
         db.session.commit()
         flash('Your post has been updated', 'success')
-    form.title.data = post.title
-    form.content.data = post.content
+        return redirect(url_for('post', post_id=post.id))
+    elif request.method == 'GET':
+        form.title.data = post.title
+        form.content.data = post.content
+
     return render_template('create_post.html', title='Update', form=form, legend='Update Post')
